5f7ba286b5b52376a4f020a327a243426558a4dc
jonnings <jonnings@gmail.com>
2020-12-30 11:43:53 +0100

    Update Exercise1-solution.py

--- solutions/Exercise1-solution.py
+++ solutions/Exercise1-solution.py
@@ -1,13 +1,13 @@
-# 1a
+# Exercise 1.5.1:
 sequence1 = "ACTG"
 sequence2 = "TCCG"
 joined_sequences = sequence1 + sequence2
 print(joined_sequences)
 print("Length: ", len(joined_sequences))
 
+# Exercise 1.5.2: The program prints 'Hi'
 
-
-# 1b
+# Exercise 1.5.3:
 A = 2
 C = 4
 G = 3
@@ -18,7 +18,7 @@ melting_temperature = 4 * (G + C) + 2 * (A + T)
 print("Melting temperature: ", melting_temperature)
 
 
-# 1c
+# Exercise 1.5.4:
 
 def compute_melting_temperature(A, C, G, T):
     return 4 * (G + C) + 2 * (A + T)
@@ -26,7 +26,7 @@ def compute_melting_temperature(A, C, G, T):
 print("Melting temperature: ", compute_melting_temperature(2, 4, 3, 5))
 
 
-# 1 d
+# Exercise 1.5.5:
 # Corrected function below
 # The error was that the return True was intented one step too much.
 # The idea here is that we want to return False if any of the bases are invalid. The code inside the function will then stop being executed.
@@ -41,12 +41,12 @@ def sequence_is_valid_dna(sequence):
 print(sequence_is_valid_dna("ACTG"))
 print(sequence_is_valid_dna("YACT"))
 
-
-# The if sentence can be written has
+# Bonus task:
+# The if sentence can be written as:
 # if base not in ["A", "C", "T", "G", "N"]:
 
 
-# 1e
+# Exercise 1.5.6:
 
 def compute_gc_content(sequence):
     number_of_g_and_c = 0
@@ -62,7 +62,7 @@ print(compute_gc_content("ACTACACTTAGGACCAC"))
 
 
 
-# 1f
+# Exercise 1.5.7:
 
 f = open("open_chromatin_chr6.bed")
 n_big_regions = 0
